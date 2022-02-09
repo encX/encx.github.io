f4143c371fa64bcb44c115a8f829a7fafe5f9aff
encX <plai.rt.w@gmail.com>
2022-02-09 18:22:00 +0700

    Update page

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,7 @@
 
 <main>
 	<h1>Hello {name}!</h1>
+	<h1>I am swelte dev now!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
