be4f5313da45f5c997f4c025ee23970e3bf4de5c
Plai <plai.rt.w@gmail.com>
2022-02-20 23:01:55 +0700

    TypeScripify project

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,13 @@
-<script>
-	export let name;
+<script lang="ts">
+  export let name;
 </script>
 
 <main>
-	<h1>Test</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <h1>Test</h1>
+  <p>
+    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+    how to build Svelte apps.
+  </p>
 </main>
 
 <style lang="postcss" global>
